98f3886e6d1170daed9ce84a37f0492449f3887e
sevenxqq <fantastic772256@163.com>
2021-05-05 22:21:14 +0800

    update

---
+++ lab2/TF-IDF/src/main/java/com/xjm/IDFMR.java
@@ -0,0 +1,110 @@
+package com.xjm;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Partitioner;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+
+public class IDFMR {
+
+    private static int totalFiles = 0;
+    private static int taskNum = 3;
+
+    public static void main(String[] args) {
+        Configuration conf = new Configuration();
+        try {
+            Job job = Job.getInstance(conf); 
+            job.setJobName("IDF-job");
+            job.setJarByClass(IDFMR.class);
+            job.setMapperClass(IDFMR.IDFMapper.class);    
+            //job.setCombinerClass(IDFMR.IDFCombiner.class);
+            //job.setPartitionerClass(IDFMR.IDFPartitioner.class);  
+            job.setNumReduceTasks(taskNum);
+            job.setReducerClass(IDFMR.IDFReducer.class);
+            job.setMapOutputKeyClass(Text.class);
+            job.setMapOutputValueClass(Text.class);
+            job.setOutputKeyClass(Text.class);
+            job.setOutputValueClass(Text.class);
+            String inputPath = args[0];
+            String outputPath = args[1] + "IDF";
+            Path fileOutPath = new Path(outputPath);
+            FileSystem fileSystem = FileSystem.get(conf);
+            if (fileSystem.exists(fileOutPath)) {
+                fileSystem.delete(fileOutPath, true);
+            }  
+            FileInputFormat.addInputPath(job, new Path(inputPath));
+            FileOutputFormat.setOutputPath(job, fileOutPath);
+            job.waitForCompletion(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static class IDFMapper extends Mapper<Object, Text, Text, Text> {
+        private final Text one = new Text("1"); 
+        private Text word = new Text();
+        @Override
+        protected void setup(Mapper<Object, Text, Text, Text>.Context context)
+                throws IOException, InterruptedException {
+            totalFiles++;
+        }
+        @Override
+        protected void map(Object key, Text value, Mapper<Object, Text, Text, Text>.Context context)
+                throws IOException, InterruptedException {
+            StringTokenizer tokenizer = new StringTokenizer(value.toString());
+            while (tokenizer.hasMoreTokens()) {
+                word.set(tokenizer.nextToken());
+                context.write(word, one);
+            }
+        }
+    }
+
+    public static class IDFCombiner extends Reducer<Text, Text, Text, Text> {      
+        private final Text one = new Text("1"); 
+        @Override
+        protected void reduce(Text key, Iterable<Text> values,
+                Reducer<Text, Text, Text, Text>.Context context) throws IOException, InterruptedException {   
+            if (values == null) {
+                return;
+            }                 
+            context.write(key, one);
+        }
+    }
+
+    public static class IDFReducer extends Reducer<Text, Text, Text, Text> {
+        @Override
+        protected void reduce(Text key, Iterable<Text> values, Reducer<Text, Text, Text, Text>.Context context)
+                throws IOException, InterruptedException {
+            if (values == null) {
+                return;
+            }
+            int fileCount = 0;
+            for (Text value : values) {
+                fileCount += Integer.parseInt(value.toString());
+            }
+            double idfValue = Math.log10(1.0 * totalFiles / (fileCount + 1));
+            context.write(key, new Text(String.valueOf(idfValue)));
+        }
+    }
+
+    public static class IDFPartitioner extends Partitioner<Text, Text> {
+        @Override
+        public int getPartition(Text key, Text value, int numPartitions) {
+            String word = key.toString();
+            numPartitions = taskNum;
+            return Math.abs((word.hashCode() * 127) % numPartitions);
+        }
+    }
+
+}
